5226fe4148071063ca361c5be49371fa2c987902
Dax Gerber <dax.gerber@gmail.com>
2024-02-08 11:03:38 -0700

    posts are now being brought through and working on the likes and comments button tapped stuff.

---
+++ techSocialMediaApp/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  techSocialMediaApp
+//
+//  Created by Dax Gerber on 2/8/24.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
